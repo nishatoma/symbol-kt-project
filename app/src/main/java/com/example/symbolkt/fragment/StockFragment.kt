ef5ccdd55f4e9512ea1a1bf239966fbd89e2075d
nishatoma <nishatoma@gmail.com>
2022-01-20 01:51:40 -0500

    clean class

--- app/src/main/java/com/example/symbolkt/fragment/StockFragment.kt
+++ app/src/main/java/com/example/symbolkt/fragment/StockFragment.kt
@@ -1,12 +1,11 @@
 package com.example.symbolkt.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.symbolkt.R
 import com.example.symbolkt.databinding.FragmentStockBinding
 import com.example.symbolkt.viewmodel.StockViewModel
 
@@ -20,7 +19,7 @@ class StockFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         val binding = FragmentStockBinding.inflate(inflater)
 

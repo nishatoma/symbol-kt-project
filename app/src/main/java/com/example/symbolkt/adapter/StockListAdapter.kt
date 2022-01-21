e48137367b4eed5a160fcd9386b94f0dc888dc1c
nishatoma <nishatoma@gmail.com>
2022-01-20 21:54:56 -0500

    first screen

---
+++ app/src/main/java/com/example/symbolkt/adapter/StockListAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.symbolkt.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class StockListAdapter: RecyclerView.Adapter<StockListAdapter.StockViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StockViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: StockViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class StockViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    }
+}
+
